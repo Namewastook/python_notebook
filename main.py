9b69d997179977d8542278fe11f5a7f242fabfcc
TJ <philosophy.cheer@gmail.com>
2019-05-28 15:10:05 -0400

    Everything is def

--- main.py
+++ main.py
@@ -13,22 +13,42 @@ counter = 1
 notebook = []
 now = date.today()
 
-user_response = input("Would you like to make a note? \n"
-                      "1. Add a note\n"
-                      "2. Print a note \n"
-                      "3. Exit\n>"
-                      )
 
-if user_response == "1":
+def menu():
+    user_response = input("Would you like to make a note? \n"
+                          "1. Add a note\n"
+                          "2. Print a note \n"
+                          "3. Exit\n>"
+                          )
+    return user_response
+
+
+def note_create():
     content = input("What is the note? \n>")
+    global counter
     note_id = counter
     note = (note_id, str(now), content)
     notebook.append(note)
     counter += 1
-elif user_response == "2":
+
+
+def print_notes():
     for note in notebook:
         print(f"Id: {note[0]}|{note[2]}")
-elif user_response == "3":
-    exit()
-else:
-    print(notebook)
+
+
+def run():
+    while True:
+        choice = menu()
+        if choice == "1":
+            note_create()
+        elif choice == "2":
+            print_notes()
+        elif choice == "3":
+            exit()
+        else:
+            print(notebook)
+
+
+if __name__ == "__main__":
+    run()
