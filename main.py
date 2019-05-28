a144c8a714a1420c78720bddcfa9c7adf3d9e89f
TJ <philosophy.cheer@gmail.com>
2019-05-28 14:19:20 -0400

    Tested and made a few new notes

--- main.py
+++ main.py
@@ -8,19 +8,15 @@ A note has 3 parts:
 3. Date/time
 """
 
-notebook = []
 
 counter = 1
-note_id = counter
-
-counter += 1
-
+notebook = []
 now = date.today()
 
-content = "this is content"
-
-note = (note_id, str(now), content)
-
-notebook.append(note)
-
-print(notebook)
+while True:
+    content = input("What is the note? \n>")
+    note_id = counter
+    note = (note_id, str(now), content)
+    notebook.append(note)
+    counter += 1
+    print(notebook)
